819504e00fe46a133eac74c511c5ce300a222d1d
Proto Ford <58895172+Protoford@users.noreply.github.com>
2022-01-10 13:16:40 -0800

    Modernize
    Rewrote Resource file to FXManifest style.

---
+++ es_carwash/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- modified by Calm Productions
+
+version '0.6.2'
+author 'TheSpartaPT'
+description 'EssentialMode 5 CarWash'
+-- repository 'https://github.com/
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_scripts {
+	'es_carwash_client.lua'
+}
+
+server_scripts {
+	'es_carwash_server.lua'
+}
